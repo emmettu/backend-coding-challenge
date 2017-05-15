ac410adb6f251ae5089498adc709933591d00e1b
Emmett Underhill <emmettu@gmail.com>
2017-05-15 02:47:45 -0600

    Adding scores

--- city/models.py
+++ city/models.py
@@ -1,8 +1,51 @@
 from django.db import models
 import csv
 import sys
+import math
+import json
+from difflib import SequenceMatcher
 
 # Create your models here.
+class ScoreCalculator:
+    def __init__(self, city, query):
+
+        self.score_map = {
+            "population": self.score_population,
+            "latitude": self.score_latitude,
+            "longitude": self.score_longitude,
+            "q": self.score_name
+        }
+
+        self.query = query
+        self.city = city
+
+    def calculate_score(self):
+        total_score = 0
+        for k in self.query.keys():
+            total_score += self.score_map[k]()
+
+        return total_score / len(self.query)
+
+    def score_population(self):
+        desired_pop = int(self.query["population"])
+        city_pop = self.city.population
+        return 1 / (math.log(abs(city_pop - desired_pop) + 1) + 1)
+
+    def score_latitude(self):
+        desired_lat = float(self.query["latitude"])
+        city_lat = self.city.latitude
+        return 1 / (math.log(abs(city_lat - desired_lat) + 1) + 1)
+
+    def score_longitude(self):
+        desired_lon = float(self.query["longitude"])
+        city_lon = self.city.longitude
+        return 1 / (math.sqrt(abs(city_lon - desired_lon)) + 1)
+
+    def score_name(self):
+        desired_name = self.query["q"]
+        city_name = self.city.name
+        return SequenceMatcher(None, desired_name, city_name).ratio()
+
 
 class City(models.Model):
     name = models.TextField()
@@ -10,6 +53,16 @@ class City(models.Model):
     longitude = models.FloatField()
     population = models.IntegerField()
 
+    def calculate_score(self, query):
+        return ScoreCalculator(self, query).calculate_score()
+
+    def dictionary(self):
+        return {
+            "name": self.name,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "population": self.population
+        }
 
 def load_data():
     csv.field_size_limit(sys.maxsize)
--- city/views.py
+++ city/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from city.models import City
 
 # Create your views here.
 
 
 def suggestions(request):
-    return HttpResponse("Hello")
+    query = request.GET
+
+    cities = City.objects.all()
+
+    top_cities = sorted(cities, key=lambda x: x.calculate_score(query), reverse=True)
+    return build_response(top_cities[:10], "Query Success")
+
+
+def build_response(cities, message):
+
+    response = {
+        "suggestions": [c.dictionary() for c in cities],
+        "message": message
+    }
+
+    return JsonResponse(response)
