984c7cf369a7887a4bcafd3d485be5767cf49a0c
Emmett Underhill <emmettu@gmail.com>
2017-05-16 02:52:53 -0600

    Heroku db

--- challenge/settings.py
+++ challenge/settings.py
@@ -110,7 +110,7 @@ if db_url:
 
     # Update database configuration with $DATABASE_URL.
     db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = (db_from_env)
 
 else:
     DATABASES = {
